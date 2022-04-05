d09dd5ef4a450e9d5260e988115aab19d1ef33ff
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-04-05 12:39:32 -0700

    initial commit

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import pandas
 
 data = pandas.read_csv("data.csv")
 
-print(data['temp'])
+#print(data['temp'])
 
+data_dict = data.to_dict()
+
+print(data_dict)
+temp_list = data["temp"].to_list()
+average_temp = sum(temp_list)/ len(temp_list)
+print(average_temp)
+
+print(data['temp'].max())
 
 
